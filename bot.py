9a085dce0c2c9688d233040dad1b664f8f682606
Naveenk432 <naveenmallppakalloli@gmail.com>
2026-03-04 20:37:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import os
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = Client(
-    "thumbnailbot",
+    "thumbnail-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
@@ -16,32 +17,43 @@ bot = Client(
 user_data = {}
 
 @bot.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Send a file to start.")
+async def start(client, message: Message):
+    await message.reply_text(
+        "Hello!\n\nSend a file.\n"
+        "Then send thumbnail.\n"
+        "Then send caption."
+    )
 
 @bot.on_message(filters.document | filters.video | filters.audio)
-async def get_file(client, message):
+async def receive_file(client, message: Message):
+
     user_id = message.from_user.id
+
+    await message.reply_text("Downloading file...")
+
     file_path = await message.download()
 
     user_data[user_id] = {"file": file_path}
 
-    await message.reply_text("Send thumbnail image.")
+    await message.reply_text("Now send thumbnail image.")
 
 @bot.on_message(filters.photo)
-async def get_thumb(client, message):
+async def receive_thumb(client, message: Message):
+
     user_id = message.from_user.id
 
     if user_id not in user_data:
         return
 
-    thumb = await message.download()
-    user_data[user_id]["thumb"] = thumb
+    thumb_path = await message.download()
 
-    await message.reply_text("Send caption text.")
+    user_data[user_id]["thumb"] = thumb_path
+
+    await message.reply_text("Now send caption text.")
 
 @bot.on_message(filters.text & ~filters.command)
-async def get_caption(client, message):
+async def receive_caption(client, message: Message):
+
     user_id = message.from_user.id
 
     if user_id not in user_data:
@@ -49,19 +61,23 @@ async def get_caption(client, message):
 
     caption = message.text
     file_path = user_data[user_id]["file"]
-    thumb = user_data[user_id]["thumb"]
+    thumb_path = user_data[user_id]["thumb"]
+
+    await message.reply_text("Uploading file...")
 
     await message.reply_document(
         document=file_path,
-        thumb=thumb,
+        thumb=thumb_path,
         caption=caption
     )
 
     os.remove(file_path)
-    os.remove(thumb)
+    os.remove(thumb_path)
 
     user_data.pop(user_id)
 
     await message.reply_text("Done!")
+
+print("Bot Started Successfully")
 
 bot.run()
