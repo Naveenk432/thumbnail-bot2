921d5b86e28fa39bd7c25e538e5c48b93399d1c1
Naveenk432 <naveenmallppakalloli@gmail.com>
2026-03-20 16:12:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,48 @@
 import os
 import asyncio
 from pyrogram import Client, filters
+from aiohttp import web
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Client(
-    "test-bot",
+    "bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
+# ================= BOT =================
+
 @bot.on_message(filters.command("start"))
 async def start(client, message):
-    print("START COMMAND RECEIVED")
-    await message.reply_text("✅ Bot is working!")
+    await message.reply("✅ Bot is working!")
+
+# ================= FAKE SERVER =================
+
+async def handle(request):
+    return web.Response(text="Bot is alive")
+
+async def start_webserver():
+    app = web.Application()
+    app.router.add_get("/", handle)
+
+    port = int(os.getenv("PORT", 8080))  # Railway needs this
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+
+# ================= MAIN =================
 
 async def main():
     await bot.start()
-    print("🔥 BOT RUNNING")
+    print("🤖 Bot Running")
+
+    await start_webserver()   # 👈 IMPORTANT
+
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
